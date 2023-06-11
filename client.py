1dba3b11a38ae47f7312babd491cad41771c4167
Jostic11 <jostic11@mail.ru>
2023-06-11 23:57:13 +0300

    chat pika + rabbitmq

---
+++ client.py
@@ -0,0 +1,219 @@
+import os
+import sys
+import pika
+import grpc
+import mafia_pb2_grpc as mafia_pb2_grpc
+import mafia_pb2 as mafia_pb2
+from simple_term_menu import TerminalMenu
+from art import tprint
+from threading import Thread
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+
+class UnaryClient:
+    def __init__(self, host, port, players):
+        self.host = host
+        self.server_port = port
+        self.chanel = grpc.insecure_channel(f'{self.host}:{self.server_port}')
+        self.stub = mafia_pb2_grpc.MafiaStub(self.chanel)
+        self.room_users = []
+        self.role = None
+        self.players = players
+        self.in_chat = False
+        self.credentials = pika.PlainCredentials(os.environ["RABBITMQ_USERNAME"], os.environ["RABBITMQ_PASSWORD"])
+        self.parameters = pika.ConnectionParameters(host='localhost', port=5672, credentials=self.credentials, heartbeat=600,
+                                       blocked_connection_timeout=300)
+        self.connection = pika.BlockingConnection(parameters=self.parameters)
+        self.channel = self.connection.channel()
+
+    def subscribe_to_notifications(self):
+        self.notifications = grpc.insecure_channel(f'{self.host}:{self.server_port}')
+        try:
+            for notification in self.stub.SubscribeToNotifications(
+                            mafia_pb2.SubscribeRequest(name=self.name, game_id=self.game_id)):            
+                if (notification.msg == f"__DEAD__ {self.name}"):
+                    return
+                elif notification.msg.split()[0] == "__DEAD__":
+                    continue
+                elif notification.msg.split()[0] == "__ADD__":
+                    if notification.msg.split()[2] not in self.room_users:
+                        self.room_users.append(notification.msg.split()[2])
+                elif notification.msg.split()[0] == "__DISC__":
+                    if notification.msg.split()[2] in self.room_users:
+                        self.room_users.remove(notification.msg.split()[2])
+                elif notification.msg.split()[0] == "__MSG__":
+                    st = 1
+                    if notification.msg.split()[1] == "__MAFIA__":
+                        if self.role != "mafia":
+                            continue
+                        st = 2
+                    if self.in_chat:
+                        print(" ".join(notification.msg.split()[st:]))
+                    continue
+
+                print(notification.msg)
+        except Exception as e:
+            sys.exit()
+
+    def disconect_game(self):
+        print("You left the lobby")
+        self.stub.DisconectRoom(mafia_pb2.SingUp(name=self.name, game_id=self.game_id))
+        self.room_users.clear()
+
+    def connect_game(self, game_id):
+        response = None
+        while True:
+            response = self.stub.ConnectRoom(mafia_pb2.SingUp(name=self.name, game_id=game_id))
+            if response.game_id == "":
+                print("This game ID is busy, try another one or leave this field empty, we will create a game for you")
+                game_id = str(input())
+                continue
+            break
+        self.room_users = list(response.players)
+        self.game_id = response.game_id
+        print(f"You have connected to the room {response.game_id}")
+
+
+    def run_night(self, city):
+        print("The city is falling asleep")
+        if self.role is not None:
+            if self.role == "mafia":
+                print("Now you are in a chat with mafia, after you chat, send an empty message and you will continue to play")
+                self.in_chat = True
+                while True:
+                    tmp = str(input())
+                    if tmp == "":
+                        break
+                    tmp = f"__MAFIA__ User {self.name} send: " + tmp
+                    self.channel.basic_publish(exchange='', routing_key=self.game_id, body=tmp)
+                self.in_chat = False
+
+                print("Choose the player you want to kill:")
+                menu = TerminalMenu(city)
+                res = menu.show()
+                self.stub.KillCitizen(mafia_pb2.KillCitizenRequest(game_id=self.game_id, name=city[res]))
+            if self.role == "commissar":
+                print("Select the player you would like to test:")
+                menu = TerminalMenu(city)
+                res = menu.show()
+                self.stub.CheckCitizen(mafia_pb2.CheckCitizenRequest(game_id=self.game_id, name=city[res]))
+        response = self.stub.GetNightResult(mafia_pb2.GetNightResultRequest(game_id=self.game_id))
+        if response.is_end:
+            tprint(f"{response.end}")
+            self.notifications.close()
+            self.chanel.close()
+            self.connection.close()
+            sys.exit()
+        city = response.city
+        if (self.role is not None) and (self.name not in city):
+            self.role = None
+            print("You were killed by mafia")
+        self.run_day(city)
+
+    def run_day(self, city):
+        print("The city is waking up")
+        response = None
+        if self.role is not None:
+            print("Now you are in a chat, after you chat, send an empty message and you will continue to play")
+
+            self.in_chat = True
+            while True:
+                tmp = str(input())
+                if tmp == "":
+                    break
+                tmp = f"User {self.name} send: " + tmp
+                self.channel.basic_publish(exchange='', routing_key=self.game_id, body=tmp)
+            self.in_chat = False
+
+            print("Vote for who you think is the mafia:")
+            menu = TerminalMenu(city)
+            res = menu.show()
+            response = self.stub.CityVoting(mafia_pb2.CityVotingRequest(game_id=self.game_id, name=self.name, vote=city[res]))
+        else:
+            response = self.stub.CityVoting(mafia_pb2.CityVotingRequest(game_id=self.game_id, name=self.name, vote=self.name))
+        if response.is_end:
+            tprint(f"{response.end}")
+            self.notifications.close()
+            self.chanel.close()
+            self.connection.close()
+            sys.exit()
+        city = response.city
+        if (self.role is not None) and (self.name not in city):
+            self.role = None
+            print("You were killed by voting")
+        self.run_night(city)
+
+
+    def register_user(self, name, game_id):
+        self.name = name
+        self.game_id = game_id
+        response = None
+        while True:
+            response = self.stub.GoSingUp(mafia_pb2.SingUp(name=self.name, game_id=self.game_id))
+            if response.game_id == "":
+                print("This game ID is busy, try another one or leave this field empty, we will create a game for you")
+                self.game_id = str(input())
+                continue
+            break
+        self.game_id = response.game_id
+        print(f"User has been added to the lobby {response.game_id}")
+        new_thread = Thread(target=self.subscribe_to_notifications)
+        new_thread.start()
+        print("User is subscribed to lobby notifications")
+        for player in response.players:
+            self.room_users.append(player)
+            if player == self.name:
+                continue
+            print(f"Player {player} already in the lobby")
+        if len(self.room_users) < self.players:
+            print(f"After connecting {self.players} players, the game will automatically start")
+        else:
+            response = self.stub.GetRole(mafia_pb2.SingUp(name=self.name, game_id=self.game_id))
+            self.role = response.role
+            print(f"You are {self.role}!!!")
+            self.run_day(self.room_users)
+        while True:
+            if len(self.room_users) == self.players:
+                response = self.stub.GetRole(mafia_pb2.SingUp(name=self.name, game_id=self.game_id))
+                self.role = response.role
+                print(f"You are {self.role}!!!")
+                self.run_day(self.room_users)
+                sys.exit()
+            choice = ["List of participants in the room", "Change the room", "Exit the game", "Refresh"]
+            menu = TerminalMenu(choice)
+            res = menu.show()
+            if res == 0:
+                print(f"There are currently {len(self.room_users)} users in the lobby")
+                for i in self.room_users:
+                    print(i)
+            elif res == 1:
+                print("Enter the ID of the game you want to connect to, or leave this field empty, we will create a game for you")
+                game_id = str(input())
+                self.disconect_game()
+                response = self.connect_game(game_id)
+            elif res == 2:
+                self.stub.DeadSignal(mafia_pb2.SingUp(name=self.name, game_id=self.game_id))
+                print("You are leaving the game, goodbye!")
+                self.disconect_game()
+                new_thread.join()
+                self.notifications.close()
+                self.chanel.close()
+                self.connection.close()
+                sys.exit()
+            else:
+                continue
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("wrong number of arguments")
+        sys.exit()
+    tprint("SOA-MAFIA")
+    client = UnaryClient(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+    print("Enter your name:")
+    client.name = str(input())
+    print("Enter the ID of the game you want to connect to, or leave this field empty, we will create a game for you")
+    client.game_id = str(input())
+    client.register_user(client.name, client.game_id)
+
---
+++ server.py
@@ -0,0 +1,259 @@
+import os
+import logging
+import random
+import copy
+from concurrent import futures
+from threading import Lock
+from celery import Celery
+from threading import Thread
+
+import pika
+import grpc
+import mafia_pb2_grpc as mafia_pb2_grpc
+import mafia_pb2 as mafia_pb2
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+logger = logging.getLogger(__name__)
+
+class MafiaService(mafia_pb2_grpc.MafiaServicer):
+    def __init__(self):
+        self.users_room = {}
+        self.users_notification = {}
+        self.ready_players = {}
+        self.game_id = 0
+        self.notifications = {}
+        self.inf = -1
+        self.games_role_map = {}
+        self.games_role = {}
+        self.games_vote = {}
+        self.games_alive = {}
+        self.games_ans = {}
+        self.wait_users = {}
+        self.wait_users_unlock = {}
+        self.lock = {}
+        self.chats = {}
+        # logger.log(level=logging.WARNING, msg=f"{os.environ['RABBITMQ_USERNAME']}, {os.environ['RABBITMQ_PASSWORD']}")
+        self.credentials = pika.PlainCredentials(os.environ["RABBITMQ_USERNAME"], os.environ["RABBITMQ_PASSWORD"])
+        self.parameters = pika.ConnectionParameters(host='rabbitmq', port=5672, credentials=self.credentials, heartbeat=600,
+                                       blocked_connection_timeout=300)
+        self.connection = pika.BlockingConnection(parameters=self.parameters)
+        self.channel = self.connection.channel()
+
+    def ConnectToTheChat(self, game_id):
+        for method_frame, properties, body in self.channel.consume(game_id):
+            self.notifications[game_id].append(f"__MSG__ {body.decode()}")
+            self.channel.basic_ack(delivery_tag = method_frame.delivery_tag)
+
+    def wait_all(self, game_id):
+        with self.lock[game_id]:
+            self.wait_users[game_id] += 1
+        # logger.log(level=logging.WARNING, msg=f"wait_users+1")
+        while True:
+            with self.lock[game_id]:
+                if len(self.ready_players[game_id]) == self.wait_users[game_id]:
+                    break
+        with self.lock[game_id]:
+            self.wait_users_unlock[game_id] += 1
+        while True:
+            with self.lock[game_id]:
+                if len(self.ready_players[game_id]) == self.wait_users_unlock[game_id]:
+                    self.wait_users[game_id] -= 1
+                    break
+        # logger.log(level=logging.WARNING, msg=f"wait_users-1")
+        while True:
+            with self.lock[game_id]:
+                if self.wait_users[game_id] == 0:
+                    self.wait_users_unlock[game_id] -= 1
+                    break
+        while True:
+            with self.lock[game_id]:
+                if self.wait_users_unlock[game_id] == 0:
+                    break
+        # logger.log(level=logging.WARNING, msg=f"wait_all_end")
+
+    def GetNightResult(self, request, context):
+        self.wait_all(request.game_id)
+        is_mafia_alive = False
+        for i in self.games_alive[request.game_id]:
+            if self.games_role_map[request.game_id][i] == "mafia":
+                is_mafia_alive = True
+        if not is_mafia_alive:
+            return mafia_pb2.GetNightResultResponse(is_end=True, end="citizens won", city=self.games_alive[request.game_id])
+        if len(self.games_alive[request.game_id]) <= 2:
+            return mafia_pb2.GetNightResultResponse(is_end=True, end="the mafia won", city=self.games_alive[request.game_id])
+        return mafia_pb2.GetNightResultResponse(is_end=False, end="", city=self.games_alive[request.game_id])
+
+
+    def CheckCitizen(self, request, context):
+        is_mafia = False
+        if self.games_role_map[request.game_id][request.name] == "mafia":
+            is_mafia = True
+        if is_mafia:
+            self.notifications[self.users_room[request.name]].append(f"Commissar said: The mafia is {request.name}")
+        else:
+            self.notifications[self.users_room[request.name]].append(f"Commissar said: The mafia is NOT {request.name}")
+        return mafia_pb2.Empty()
+
+    def KillCitizen(self, request, context):
+        if  request.name in self.games_alive:
+            self.notifications[self.users_room[request.name]].append(f"The mafia killed the user {request.name}")
+            self.games_alive[request.game_id].remove(request.name)
+        return mafia_pb2.Empty()
+
+    def CityVoting(self, request, context):
+        logger.log(level=logging.WARNING, msg=f"game: {request.game_id} user {request.name} vote for: {request.vote}")
+        with self.lock[request.game_id]:
+            if request.vote in self.games_alive[request.game_id]:
+                self.games_vote[request.game_id].append(request.vote)
+        while True:
+            self.wait_all(request.game_id)
+
+            with self.lock[request.game_id]:
+                if len(self.games_vote[request.game_id]) > 0:
+                    tmp = {}
+                    for i in self.games_vote[request.game_id]:
+                        if i in tmp:
+                            tmp[i] += 1
+                        else:
+                            tmp[i] = 1
+                    mmax = 0
+                    self.games_ans[request.game_id] = ""
+                    all = []
+                    for name, num in tmp.items():
+                        all.append(num)
+                        if mmax < num:
+                            mmax = num
+                            self.games_ans[request.game_id] = name
+                    all = sorted(all)
+                    logger.log(level=logging.WARNING, msg=all)
+                    if (len(all) == 1) or (all[-1] != all[-2]):
+                        killed = self.games_ans[request.game_id]
+                        self.games_alive[request.game_id].remove(killed)
+                        self.notifications[self.users_room[request.name]].append(f"by voting, a player {self.games_ans[request.game_id]} was kicked out")
+                    else:
+                        self.games_ans[request.game_id] = ""
+                        self.notifications[request.game_id].append(f"The voices were shared, today everyone remains alive")
+                    self.games_vote[request.game_id] = []
+            # wait all
+            # self.wait_all(request.game_id)
+            # logger.log(level=logging.WARNING, msg=f"{self.games_ans[request.game_id]}")
+            if (self.games_ans[request.game_id] != "") and (self.games_role_map[request.game_id][self.games_ans[request.game_id]] == "mafia"):
+                return mafia_pb2.CityVotingResponse(is_end=True, end="citizens won", city=self.games_alive[request.game_id])
+            if len(self.games_alive[request.game_id]) <= 2:
+                return mafia_pb2.CityVotingResponse(is_end=True, end="the mafia won", city=self.games_alive[request.game_id])
+            return mafia_pb2.CityVotingResponse(is_end=False, end="", city=self.games_alive[request.game_id])
+
+    def GetRole(self, request, context):
+        logger.log(level=logging.WARNING, msg=f"user {request.name} GetRole, game: {request.game_id}")
+        with self.lock[request.game_id]:
+            if request.game_id not in self.games_role:
+                self.games_role[request.game_id] = ["citizen", "citizen", "mafia", "commissar"]
+                now = 0
+                while len(self.games_role[request.game_id]) < len(self.ready_players[request.game_id]):
+                    if now % 2 == 0:
+                        self.games_role[request.game_id].append("citizen")
+                    else:
+                        self.games_role[request.game_id].append("mafia")
+                    now += 1
+                self.games_alive[request.game_id] = copy.deepcopy(self.ready_players[request.game_id])
+                self.games_role_map[request.game_id] = {}
+                random.shuffle(self.games_role[request.game_id])
+            for i in range(len(self.ready_players[request.game_id])):
+                self.games_role_map[request.game_id][self.ready_players[request.game_id][i]] = self.games_role[request.game_id][i]
+                if request.name == self.ready_players[request.game_id][i]:
+                    logger.log(level=logging.WARNING, msg=f"game: {request.game_id} user {request.name} get role: {self.games_role[request.game_id][i]}")
+                    return mafia_pb2.GetRoleResponse(role=self.games_role[request.game_id][i])
+
+    def SubscribeToNotifications(self, request, context):
+        logger.log(level=logging.WARNING, msg=f"user {request.name} SubscribeToNotifications for room: {request.game_id}")
+        while True:
+            if (self.inf == self.users_notification[request.name]) or (self.users_notification[request.name] >= len(self.notifications[self.users_room[request.name]])):
+                continue
+            num = self.users_notification[request.name]
+            notif = self.notifications[self.users_room[request.name]]
+            yield mafia_pb2.SubscribeResponse(msg=notif[num])
+            self.users_notification[request.name] += 1
+
+    def DeadSignal(self, request, context):
+        self.notifications[self.users_room[request.name]].append(f"__DEAD__ {request.name}")
+        return mafia_pb2.Empty()
+    
+    def DisconectRoom(self, request, context):
+        logger.log(level=logging.WARNING, msg=f"user {request.name} DisconectRoom {request.game_id}")
+        self.users_notification[request.name] = self.inf
+        self.ready_players[request.game_id].remove(request.name)
+        self.notifications[request.game_id].append(f"__DISC__ User {request.name} disconnected the game")
+        return mafia_pb2.Empty()
+    
+    def ConnectRoom(self, request, context):
+        logger.log(level=logging.WARNING, msg=f" ConnectRoom received")
+        if (request.game_id.strip() == ""):
+            while True:
+                self.game_id += 1
+                request.game_id = str(self.game_id)
+                if (request.game_id not in self.games_alive) or (len(self.games_alive[request.game_id]) == 0):
+                    break
+            logger.log(level=logging.WARNING, msg=f"SERVER: user {request.name} create the game {request.game_id}")
+        else:
+            logger.log(level=logging.WARNING, msg=f"SERVER: user {request.name} try to sing up in game {request.game_id}")
+        if (request.game_id in self.games_alive) and (len(self.games_alive[request.game_id]) > 0):
+            return mafia_pb2.SingUpResponse(game_id="", players=self.ready_players[request.game_id])
+        if request.game_id in self.ready_players:
+            self.ready_players[request.game_id].append(request.name)
+            self.notifications[request.game_id].append(f"__ADD__ User {request.name} connected the game")
+        else:
+            self.ready_players[request.game_id] = [request.name]
+            self.notifications[request.game_id] = []
+            self.games_vote[request.game_id] = []
+            self.wait_users[request.game_id] = 0
+            self.wait_users_unlock[request.game_id] = 0
+            self.lock[request.game_id] = Lock()
+            self.channel.queue_declare(queue=request.game_id)
+            self.chats[request.game_id] = Thread(target=self.ConnectToTheChat)
+            self.chats[request.game_id].start()
+        self.users_room[request.name] = request.game_id
+        self.users_notification[request.name] = len(self.notifications[request.game_id])
+        return mafia_pb2.SingUpResponse(game_id=request.game_id, players=self.ready_players[request.game_id])
+
+    def GoSingUp(self, request, context):
+        logger.log(level=logging.WARNING, msg=f"user {request.name} GoSingUp received")
+        if (request.game_id.strip() == ""):
+            while True:
+                self.game_id += 1
+                request.game_id = str(self.game_id)
+                if (request.game_id not in self.games_alive) or (len(self.games_alive[request.game_id]) == 0):
+                    break
+            logger.log(level=logging.WARNING, msg=f"SERVER: user {request.name} create the game {request.game_id}")
+        else:
+            logger.log(level=logging.WARNING, msg=f"SERVER: user {request.name} try to sing up in game {request.game_id}")
+        if (request.game_id in self.games_alive) and (len(self.games_alive[request.game_id]) > 0):
+            return mafia_pb2.SingUpResponse(game_id="", players=self.ready_players[request.game_id])
+        if request.game_id in self.ready_players:
+            self.ready_players[request.game_id].append(request.name)
+            self.notifications[request.game_id].append(f"__ADD__ User {request.name} connected the game")
+        else:
+            self.ready_players[request.game_id] = [request.name]
+            self.notifications[request.game_id] = []
+            self.games_vote[request.game_id] = []
+            self.wait_users[request.game_id] = 0
+            self.wait_users_unlock[request.game_id] = 0
+            self.lock[request.game_id] = Lock()
+            self.channel.queue_declare(queue=request.game_id)
+            self.chats[request.game_id] = Thread(target=self.ConnectToTheChat, args=[request.game_id])
+            self.chats[request.game_id].start()
+        self.users_room[request.name] = request.game_id
+        self.users_notification[request.name] = len(self.notifications[request.game_id])
+        return mafia_pb2.SingUpResponse(game_id=request.game_id, players=self.ready_players[request.game_id])
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=20))
+    service = MafiaService()
+    mafia_pb2_grpc.add_MafiaServicer_to_server(service, server)
+    server.add_insecure_port('[::]:8000')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
